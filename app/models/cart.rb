a993cb16d63fdd0c0f36839e09fd64d891f439f0
Francesco Pezzella <franzpezzella@gmail.com>
2015-08-25 15:03:38 +0200

    Add price to line_item table.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,11 +2,12 @@ class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
   def add_product(product_id)
+    price = Product.find(product_id).price
     current_item = line_items.find_by(product_id: product_id)
     if current_item
       current_item.quantity += 1
     else
-      current_item = line_items.build(product_id: product_id)
+      current_item = line_items.build(product_id: product_id, price: price)
     end
     current_item
   end
---
+++ db/migrate/20150825125413_add_product_price_to_line_item.rb
@@ -0,0 +1,12 @@
+class AddProductPriceToLineItem < ActiveRecord::Migration
+  def up
+    add_column :line_items, :price, :decimal, :precision => 8, :scale => 2
+    LineItem.find(:all).each do |line_item|
+      line_item.update_attribute :price, line_item.product.price
+    end
+  end
+
+  def down
+    remove_column :line_items, :price
+  end
+end
