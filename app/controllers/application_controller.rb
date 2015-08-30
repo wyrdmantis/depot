d0b27744afc1150a080976f6f59f77c35a63e9f5
Francesco Pezzella <franzpezzella@gmail.com>
2015-08-30 18:36:28 +0200

    Set up i18n support.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < ActionController::Base
 
   # allow creation of admin account if none is set
   before_action :authorize, unless: lambda { User.no_admins? }
+  before_action :set_i18n_locale_from_params
 
   protected
 
   def authorize
     redirect_to login_url, notice: "Please log in" unless User.find_by(id: session[:user_id])
+  end
+
+  def set_i18n_locale_from_params
+    if params[:locale]
+      if I18n.available_locales.map(&:to_s).include?(params[:locale])
+        I18n.locale = params[:locale]
+      else
+        flash.now[:notice] = "#{params[:locale]} translation not available."
+        logger.error flash.now[:notice]
+      end
+    end
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,32 @@
 Depot::Application.routes.draw do
   get "admin", to: "admin#index"
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
+  get "sessions/create"
+  get "sessions/destroy"
+
   resources :users
-  resources :orders
-  resources :line_items do
-    member do
-      post 'decrement'
-    end
-  end
-  resources :carts
+
   resources :products do
     get :who_bought, on: :member
   end
 
-  get "store/index"
-  root 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items do
+      member do
+        post 'decrement'
+      end
+    end
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
